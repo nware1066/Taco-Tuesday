318c68b692f12ee977f7cf4cd29ea23a2910f75c
Naomi Ware <nware1066@yahoo.com>
2020-09-19 15:19:07 -0600

    Include get taco button in conditional render

--- src/CurrentTaco/CurrentTaco.js
+++ src/CurrentTaco/CurrentTaco.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './currentTaco.css'
 import taco3 from '../Assets/taco3.svg';
 import taco2 from '../Assets/taco2.svg';
 
@@ -7,7 +8,7 @@ function CurrentTaco(props) {
     return (
       <section>
         <h1>{props.taco.base_layer.name} Taco</h1>
-        <img src={taco2} />
+        <img src={taco2} className="taco-icon" alt="taco"/>
         <p>{props.taco.base_layer.name}</p>
         {props.taco.mixin &&
           <p>with {props.taco.mixin.name}</p>
@@ -18,13 +19,14 @@ function CurrentTaco(props) {
         {props.taco.shell &&
           <p>on {props.taco.shell.name}</p>
         }
+        <button className="taco-button" onClick={ event => props.getTaco(event)}>Get Me A Different Taco!</button>
 
       </section>
     )
   } else {
     return (
       <section>
-      <img src={taco3} className="App-logo" alt="logo" />
+      <img src={taco3} className="taco-icon" alt="taco" />
       <button className="taco-button" onClick={ event => props.getTaco(event)}>Get Me A Taco!</button>
       </section>
     )
